8ea32bf3bd71ec3f7682a035d4789528c5d7ac03
radang2014 <johnjonescomorgnetus@gmail.com>
2023-11-22 09:18:35 -0500

    Add checkpoint files to CS 40 Escape Room

--- pages/unrelated/cs40_escape/escape_start.c
+++ pages/unrelated/cs40_escape/escape_start.c
@@ -1,3 +1,30 @@
+/* 
+ * escape_start.c
+ * 
+ * Welcome to the beginning of the CS 40 Escape Room Challenge! 
+ * 
+ * You are in an escape room and are tasked with solving a series of puzzles 
+ * in order to break out. Doing so may require working solutions of all CS 40 
+ * assignments (filesofpix, iii, locality, arith, bomb, um, profiling, and asmcoding) 
+ * and general knowledge of CS 40 concepts.
+ * 
+ * The starting files for the challenge are:
+ *  - escape_start: Compiled executable
+ *  - escape_start.c: This file; the driver file for escape_start
+ * 
+ * Acknowledgements:
+ * - Elliot Bonner for creating a similar puzzle that inspired this one.
+ * - Megan Monroe for writing a PGM image corruptor that I used to help make part of 
+ *   this puzzle.
+ * - Jasper Geer for providing a UM-C to UMASM compiler which I used to help make 
+ *   part of this puzzle (link here: https://github.com/jaspergeer/jumcc/).
+ * - Dan Bergen for helping test this out.
+ * 
+ * Written by: Randy Dang, Tufts University, November 2023
+ * 
+ * Good luck, and have fun! :)
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -27,4 +54,6 @@ int main()
         check_password(input);
 
         free(input);
+
+        return EXIT_SUCCESS;
 }
